a9efbe662f306babb805934e6b78ed5eb7ec08fc
alcadox <antiminas@protonmail.com>
2025-11-27 13:05:03 +0100

    Inicialización del código base del explorador de archivos

--- Lexora/MainForm.Designer.cs
+++ Lexora/MainForm.Designer.cs
@@ -36,11 +36,17 @@
             this.btnSesion = new System.Windows.Forms.Button();
             this.panelIzquierdo = new System.Windows.Forms.Panel();
             this.panelIzquierdoArchivos = new System.Windows.Forms.Panel();
+            this.btnDiscoPrincipal = new System.Windows.Forms.Button();
             this.panelArchivos = new System.Windows.Forms.Panel();
             this.listViewArchivos = new System.Windows.Forms.ListView();
+            this.columnaNombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnaTipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnaTamaño = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnaCreacion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panelTop.SuspendLayout();
             this.panelBuscador.SuspendLayout();
             this.panelIzquierdo.SuspendLayout();
+            this.panelIzquierdoArchivos.SuspendLayout();
             this.panelArchivos.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -119,10 +125,23 @@
             // panelIzquierdoArchivos
             // 
             this.panelIzquierdoArchivos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(167)))), ((int)(((byte)(255)))));
+            this.panelIzquierdoArchivos.Controls.Add(this.btnDiscoPrincipal);
             this.panelIzquierdoArchivos.Location = new System.Drawing.Point(12, 6);
             this.panelIzquierdoArchivos.Name = "panelIzquierdoArchivos";
             this.panelIzquierdoArchivos.Size = new System.Drawing.Size(167, 572);
             this.panelIzquierdoArchivos.TabIndex = 2;
+            // 
+            // btnDiscoPrincipal
+            // 
+            this.btnDiscoPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(141)))), ((int)(((byte)(227)))));
+            this.btnDiscoPrincipal.ForeColor = System.Drawing.Color.White;
+            this.btnDiscoPrincipal.Location = new System.Drawing.Point(11, 31);
+            this.btnDiscoPrincipal.Name = "btnDiscoPrincipal";
+            this.btnDiscoPrincipal.Size = new System.Drawing.Size(133, 28);
+            this.btnDiscoPrincipal.TabIndex = 1;
+            this.btnDiscoPrincipal.Text = "button1";
+            this.btnDiscoPrincipal.UseVisualStyleBackColor = false;
+            this.btnDiscoPrincipal.Click += new System.EventHandler(this.btnDiscoPrincipal_Click);
             // 
             // panelArchivos
             // 
@@ -135,12 +154,41 @@
             // 
             // listViewArchivos
             // 
+            this.listViewArchivos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnaNombre,
+            this.columnaTipo,
+            this.columnaTamaño,
+            this.columnaCreacion});
+            this.listViewArchivos.FullRowSelect = true;
+            this.listViewArchivos.GridLines = true;
             this.listViewArchivos.HideSelection = false;
             this.listViewArchivos.Location = new System.Drawing.Point(21, 6);
             this.listViewArchivos.Name = "listViewArchivos";
             this.listViewArchivos.Size = new System.Drawing.Size(942, 572);
             this.listViewArchivos.TabIndex = 0;
             this.listViewArchivos.UseCompatibleStateImageBehavior = false;
+            this.listViewArchivos.View = System.Windows.Forms.View.Details;
+            this.listViewArchivos.DoubleClick += new System.EventHandler(this.listViewArchivos_DoubleClick);
+            // 
+            // columnaNombre
+            // 
+            this.columnaNombre.Text = "Nombre";
+            this.columnaNombre.Width = 400;
+            // 
+            // columnaTipo
+            // 
+            this.columnaTipo.Text = "Tipo";
+            this.columnaTipo.Width = 150;
+            // 
+            // columnaTamaño
+            // 
+            this.columnaTamaño.Text = "Tamaño";
+            this.columnaTamaño.Width = 97;
+            // 
+            // columnaCreacion
+            // 
+            this.columnaCreacion.Text = "Fecha de Creación";
+            this.columnaCreacion.Width = 240;
             // 
             // MainForm
             // 
@@ -159,6 +207,7 @@
             this.panelBuscador.ResumeLayout(false);
             this.panelBuscador.PerformLayout();
             this.panelIzquierdo.ResumeLayout(false);
+            this.panelIzquierdoArchivos.ResumeLayout(false);
             this.panelArchivos.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -176,5 +225,10 @@
         private System.Windows.Forms.Panel panelIzquierdoArchivos;
         private System.Windows.Forms.Panel panelArchivos;
         private System.Windows.Forms.ListView listViewArchivos;
+        private System.Windows.Forms.Button btnDiscoPrincipal;
+        private System.Windows.Forms.ColumnHeader columnaNombre;
+        private System.Windows.Forms.ColumnHeader columnaTipo;
+        private System.Windows.Forms.ColumnHeader columnaTamaño;
+        private System.Windows.Forms.ColumnHeader columnaCreacion;
     }
 }
