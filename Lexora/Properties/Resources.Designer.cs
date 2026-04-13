48f6b24112120a96bde4a0831683d9980f46cdeb
alcadox <antiminas@protonmail.com>
2026-04-13 22:14:35 +0200

    ACTUALIZACION VISUAL IMPORTANTE: Al cargar las carpetas y archivos se cargan junto con sus iconos REALES, de forma optimizada y eficiente, tanto carpetas como archivos

--- Lexora/Properties/Resources.Designer.cs
+++ Lexora/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Lexora.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,26 @@ namespace Lexora.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap carpeta_back_por_defecto_w11 {
+            get {
+                object obj = ResourceManager.GetObject("carpeta_back_por_defecto_w11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap carpeta_por_defecto_w11 {
+            get {
+                object obj = ResourceManager.GetObject("carpeta_por_defecto_w11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
